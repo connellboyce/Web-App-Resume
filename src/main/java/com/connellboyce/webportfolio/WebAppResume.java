cd161134d70aa7418ee94f43a2e9f1c9118154ff
Connell Boyce <connell.p.boyce@gmail.com>
2021-01-14 15:48:01 -0500

    Set up beginnings of Thymeleaf
    - Created HTML files for each NavBar item
    - Created a controller to handle all NavBar links
    - Moved SNS Controller items to its own specific class
      - Why? -> It did not make sense to have code belonging to a controller
        in a non-controller class

--- src/main/java/com/connellboyce/webportfolio/WebAppResume.java
+++ src/main/java/com/connellboyce/webportfolio/WebAppResume.java
@@ -1,13 +1,7 @@
 package com.connellboyce.webportfolio;
 
-import com.amazonaws.services.sns.AmazonSNSAsync;
-import com.amazonaws.services.sns.model.PublishRequest;
-import com.amazonaws.services.sns.model.SubscribeRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,28 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class WebAppResume {
-	@Autowired
-	private AmazonSNSAsync amazonSNSClient;
 
-	String TOPIC_ARN = "arn:aws:sns:us-east-1:641570163578:Connell-Portfolio-Topic";
-
-	@GetMapping("/subscribe/{email}")
-	public String addSubscription(@PathVariable String email) {
-		SubscribeRequest request = new SubscribeRequest(TOPIC_ARN, "email", email);
-		amazonSNSClient.subscribe(request);
-		return "Subscription pending. Please wait for e-mail confirmation. This might take a few minutes.";
-	}
-
-	@GetMapping("/sendEmail")
-	public String publishMessage() {
-		PublishRequest publishRequest = new PublishRequest(TOPIC_ARN,buildEmailBody(),"Notification: Amazon SNS");
-		amazonSNSClient.publish(publishRequest);
-		return "Notification published.";
-	}
-
-	private String buildEmailBody() {
-		return "This is the sample email response.";
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(WebAppResume.class, args);
 	}
